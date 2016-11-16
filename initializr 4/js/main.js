ede75024e722f3cbbe0f2f38166bb7f9f7a17cae
Tess Cagle <Tess@wireless-10-145-134-12.public.utexas.edu>
2016-11-15 19:46:10 -0600

    Last steps

--- initializr 4/js/main.js
+++ initializr 4/js/main.js
@@ -6,11 +6,16 @@ function unhighlight() {
 $('h1').text('Hello World!');
 $('.jumbotron').removeClass('highlighted');
 }
-
 function hide() {
 $('.jumbotron').fadeOut();
 }
-
-function show {
-$('.jumbotron').slideDown(); 
+function show() {
+$('.jumbotron').slideDown();
 }
+
+$(document).ready(function() {
+  $('.jumbotron').on('mouseenter', highlight);
+  $('.jumbotron').on('mouseleave', unhighlight);
+  $('.hide-jumbotron').on('click', hide);
+  $('.show-jumbotron').on('click', show);
+});
