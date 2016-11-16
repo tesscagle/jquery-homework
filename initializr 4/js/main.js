09e7297d06cffe388a1a07bff661c1d9cda2289b
Tess Cagle <Tess@wireless-10-145-134-12.public.utexas.edu>
2016-11-15 19:09:07 -0600

    Step 7

--- initializr 4/js/main.js
+++ initializr 4/js/main.js
@@ -2,3 +2,7 @@ function highlight() {
 $('h1').text('Hello');
 $('.jumbotron').addClass('highlighted');
 }
+function unhighlight() {
+$('h1').text('Hello World!');
+$('.jumbotron').removeClass('highlighted');
+}
